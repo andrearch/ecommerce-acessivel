6ccd205ba3501edf867a83d32abea7544ef19e66
SrMouraSilva <mateus.moura@hotmail.com>
2018-02-22 13:24:28 -0300

    Mudanças requisitadas pelo Bosco

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,3 +8,13 @@ document.querySelector('#contraste').onclick = () => {
   
   localStorage.setItem("contraste", classes.contains('contraste'));
 }
+
+var botao = document.querySelector('#adicionar-carrinho')
+
+if (botao !== null) {
+  botao.onclick = () => {
+    var link = document.querySelector("#peca-adicionada")
+    link.classList.remove("not-exists")
+    link.focus()
+  }
+}
